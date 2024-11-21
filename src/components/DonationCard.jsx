753459c7b514e3f36039f16c546fefd680a98ff0
Jahid458 <jahidul898@outlook.com>
2024-11-21 07:26:15 +0600

    card added

--- src/components/DonationCard.jsx
+++ src/components/DonationCard.jsx
@@ -4,10 +4,11 @@ import { NavLink } from "react-router-dom";
 const DonationCard = ({ donation }) => {
   
     console.log(donation)
+  // eslint-disable-next-line no-unused-vars
   const {image,title,description,division} = donation;
   //  console.log(image,title,description,division)
   return (
-    <div className="card card-compact bg-base-100 lg:w-96 mx-auto w-[380px] md:w-[355px] shadow-xl">
+    <div className="card card-compact bg-base-100 lg:w-[500px] lg:h-[500px] mx-auto w-[380px] md:w-[355px] shadow-xl">
     
       <figure>
         <img
@@ -16,9 +17,9 @@ const DonationCard = ({ donation }) => {
         />
       </figure>
       <div className="card-body">
-        <h2 className="card-title text-2xl">{donation.title}</h2>
+        <h2 className="card-title text-2xl ">{donation.title}</h2>
         <p className="text-gray-400">{description}</p>
-        <h1 className="text-xl">Division:{division}</h1>
+        <h1 className="text-xl lg:border-2 lg:p-3 lg:font-bold ">Division:{division}</h1>
         <div className="card-actions justify-center">
           <NavLink to={`/donationdetails/${donation.id}`}>
           <button className="btn btn-primary mt-2">Donate Now</button>
